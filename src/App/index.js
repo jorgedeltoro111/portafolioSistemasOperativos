87b8e2349e5c8263029b5d99d88580fccda49790
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-09-10 10:49:07 -0600

    Agregue la actividad 4 de dispositivos fisicos

--- src/App/index.js
+++ src/App/index.js
@@ -8,7 +8,7 @@ const object = [
   {title: 'El hardware de la computadora y el sistema operativo', subtitle: 'A01', description: 'Investigación del proceso de encendido de una máquina fisica', link: 'https://drive.google.com/file/d/1mByg8B-VHjfiErVPQLSRhQ0MzVuNoMDY/view?usp=drive_link'},
   {title: 'Sistemas operativos y sus tipos', subtitle: 'A02', description: 'Comparación de sistemas operativos', link: 'https://drive.google.com/file/d/1tmlbACUlbngRNNBfcEpDrgX-85Nrps-K/view?usp=drive_link'},
   {title: 'Configuración de dispositivos', subtitle: 'A03', description: 'Investigación de USB booteable', link: 'https://drive.google.com/file/d/1TAWQhEgJRONwwCh26V9_Qt2KwgH6mFni/view'},
-
+  {title: 'Dispositivos fisicos', subtitle: 'A04', description: 'Tipos de dispositivos fisicos', link: 'https://drive.google.com/file/d/10ZGyrfh8WtrqtFjBAqUNachylm_75XV6/view?usp=drive_link'},
 ];
 
 function App() {
