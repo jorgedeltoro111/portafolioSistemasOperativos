f97a116705c3008146772966e0fe896dccba182e
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-09-19 21:16:32 -0600

    Agregando conclusion

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
       <h4 className='title text-center font-italic'>Conclusiones</h4>
       <div className='ml-5 mt-3'>
         <h5 className='font-weight-bold'>Unidad temática 1: Instalación de Sistemas Operativos</h5>
-        <p></p>
+        <p>En esta primera unidad temática se tocaron temas sumamente importantes y fundamentales para el aprendizaje de los sistemas operativos. En primer lugar, pudimos lograr comprender las partes del hardware y como se comunican con el sistema operativo. En segundo lugar, logramos identificar diferentes sistemas operativos y las diferencias entre sí, con esto teniendo un panorama mas completo de la variedad de los sistemas operativos con los que podemos trabajar. Como cultura general acerca de la tecnología identificamos diferentes tipos de dispositivos físicos que han marcado una gran diferencia en la evolución en el transcurso de la tecnología. Como 5ta actividad pasamos a la mas divertida que para mi fue la mas practica donde realizamos diferentes particiones en una maquina virtual para poder instalar 3 sistemas operativos y ponerlos en funcionamiento para poder tener un triple boot en una maquina física para que así el mismo sistema cada vez que iniciemos la computadora nos pregunte en que sistema operativo queremos iniciar. La creación del portafolio de evidencias me gusto mucho dado que es muy interactivo la creación de un portafolio y a mí me gusta mucho programar páginas web.</p>
         <h5 className='font-weight-bold'>Unidad temática 2: Configuración de Servicios</h5>
         <p></p>
         <h5 className='font-weight-bold'>Unidad temática 3: Optimización</h5>
